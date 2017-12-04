6d77807544cfeb54b24caa804e6b8d0976fcaafa
Logan Tibbetts <Lcubed37@gmail.com>
2017-12-03 22:39:52 -0600

    Changed code for calculating the index

--- yo_indexer/analyzer.py
+++ yo_indexer/analyzer.py
@@ -1,32 +1,56 @@
 """Main module for yo_indexer"""
 import csv
 import os
-from pkg_resources import resource_listdir
 
 def examine_csv():
     """The main function"""
-    # Gets a list of all of the files in the csv_files dir
-    csv_dir = resource_listdir('yo_indexer.csv_files', '')
+    # Count the Yo Index
+    count = 0
 
-    # Loop through them
-    for file_name in csv_dir:
+    # Get the system file path
+    file_name = "Yo.csv"
+    file_path = os.path.join(os.path.dirname(__file__), 'csv_files/' + file_name)
+    with open(file_path) as file:
+        reader = csv.reader(file, delimiter=',', quotechar='|')
+        for row in reader:
+            for entry in row:
+                has_data = len(entry)
+                if has_data != 0:
+                    count += 1
+    yo_index = count
+    print('Yo index:' + str(yo_index))
 
-        # If it is a .csv then do stuff
-        if file_name[-4:] == '.csv':
-            print('Examining - ' + file_name)
+    # Compare to other group files
+    files = ['GitHub.csv']
+    for file_name in files:
+        file_path = os.path.join(os.path.dirname(__file__), 'csv_files/' + file_name)
 
-            # Get the system file path
-            file_path = os.path.join(os.path.dirname(__file__), 'csv_files/' + file_name)
+        count = 0
+        with open(file_path) as csvfile:
+            reader = csv.reader(csvfile, delimiter=',', quotechar='|')
 
-            # Open the file
-            with open(file_path) as csvfile:
-                # Make csv reader
-                reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            # Features = number of cells
+            for row in reader:
+                for entry in row:
+                    has_data = len(entry)
+                    if has_data != 0:
+                        count += 1
 
-                # Output
-                for row in reader:
-                    print(row)
-                    print('==================================')
-                    print(len(row))
-        else:
-            print('Skipping - ' + file_name)
+        print("Number of features for " + file_name + ': ' + str(count))
+        print("Yo index for " + file_name + ': ' + str(count/yo_index))
+
+    # Compare to our files (different format)
+    files = ['Gmail.csv', 'Windows Mail.csv', 'Windows Outlook.csv']
+    for file_name in files:
+        file_path = os.path.join(os.path.dirname(__file__), 'csv_files/' + file_name)
+
+        count = 0
+        with open(file_path) as csvfile:
+            reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+
+            # Features = number of rows
+            for row in reader:
+                count += 1
+
+        print("Number of features for " + file_name + ': ' + str(count))
+        print("Yo index for " + file_name + ': ' + str(count/yo_index))
